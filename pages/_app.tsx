c08711996388b8413c6e072fafebebe96c800f04
Michal-W-dev <maikey20@gmail.com>
2022-02-18 19:44:14 +0100

    Npm: bootstrap, sass, classnames, fontawesome.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
